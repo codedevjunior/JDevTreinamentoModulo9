1f941f32576b3796fd994a18742f5b9dd1ee142f
codedevjunior <code.devjunior@outlook.com.br>
2022-02-03 22:11:08 -0300

    Entendendo métodos com retorno, sem retorno, solucionando problema nullpointerexception, estrada de dados teclado, conhecendo toString, conhecendo Equals e HashCode

--- src/br/com/codedevjunior/model/Aluno.java
+++ src/br/com/codedevjunior/model/Aluno.java
@@ -1,4 +1,200 @@
 package br.com.codedevjunior.model;
 
+import java.util.Objects;
+
 public class Aluno {
+
+    private String nome;
+    private int idade;
+    private String dataNascimento;
+    private String registroGeral;
+    private String numeroCpf;
+    private String nomeMae;
+    private String nomePai;
+    private String dataMatricula;
+    private String nomeEscola;
+    private String serieMatriculado;
+    private double nota1;
+    private double nota2;
+    private double nota3;
+    private double nota4;
+
+    public Aluno(){
+
+    }
+
+    public Aluno(String nomePadrao){
+        nome = nomePadrao;
+    }
+
+    public Aluno(String nomePadrao, int idadePadrao){
+        nome = nomePadrao;
+        idade = idadePadrao;
+    }
+
+    /**
+     * Methods Getters e Setters
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getRegistroGeral() {
+        return registroGeral;
+    }
+
+    public void setRegistroGeral(String registroGeral) {
+        this.registroGeral = registroGeral;
+    }
+
+    public String getNumeroCpf() {
+        return numeroCpf;
+    }
+
+    public void setNumeroCpf(String numeroCpf) {
+        this.numeroCpf = numeroCpf;
+    }
+
+    public String getNomeMae() {
+        return nomeMae;
+    }
+
+    public void setNomeMae(String nomeMae) {
+        this.nomeMae = nomeMae;
+    }
+
+    public String getNomePai() {
+        return nomePai;
+    }
+
+    public void setNomePai(String nomePai) {
+        this.nomePai = nomePai;
+    }
+
+    public String getDataMatricula() {
+        return dataMatricula;
+    }
+
+    public void setDataMatricula(String dataMatricula) {
+        this.dataMatricula = dataMatricula;
+    }
+
+    public String getNomeEscola() {
+        return nomeEscola;
+    }
+
+    public void setNomeEscola(String nomeEscola) {
+        this.nomeEscola = nomeEscola;
+    }
+
+    public String getSerieMatriculado() {
+        return serieMatriculado;
+    }
+
+    public void setSerieMatriculado(String serieMatriculado) {
+        this.serieMatriculado = serieMatriculado;
+    }
+
+    public double getNota1() {
+        return nota1;
+    }
+
+    public void setNota1(double nota1) {
+        this.nota1 = nota1;
+    }
+
+    public double getNota2() {
+        return nota2;
+    }
+
+    public void setNota2(double nota2) {
+        this.nota2 = nota2;
+    }
+
+    public double getNota3() {
+        return nota3;
+    }
+
+    public void setNota3(double nota3) {
+        this.nota3 = nota3;
+    }
+
+    public double getNota4() {
+        return nota4;
+    }
+
+    public void setNota4(double nota4) {
+        this.nota4 = nota4;
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno{" +
+                "nome='" + nome + '\'' +
+                ", idade=" + idade +
+                ", dataNascimento='" + dataNascimento + '\'' +
+                ", registroGeral='" + registroGeral + '\'' +
+                ", numeroCpf='" + numeroCpf + '\'' +
+                ", nomeMae='" + nomeMae + '\'' +
+                ", nomePai='" + nomePai + '\'' +
+                ", dataMatricula='" + dataMatricula + '\'' +
+                ", nomeEscola='" + nomeEscola + '\'' +
+                ", serieMatriculado='" + serieMatriculado + '\'' +
+                ", nota1=" + nota1 +
+                ", nota2=" + nota2 +
+                ", nota3=" + nota3 +
+                ", nota4=" + nota4 +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Aluno aluno = (Aluno) o;
+        return idade == aluno.idade && Double.compare(aluno.nota1, nota1) == 0 && Double.compare(aluno.nota2, nota2) == 0 && Double.compare(aluno.nota3, nota3) == 0 && Double.compare(aluno.nota4, nota4) == 0 && Objects.equals(nome, aluno.nome) && Objects.equals(dataNascimento, aluno.dataNascimento) && Objects.equals(registroGeral, aluno.registroGeral) && Objects.equals(numeroCpf, aluno.numeroCpf) && Objects.equals(nomeMae, aluno.nomeMae) && Objects.equals(nomePai, aluno.nomePai) && Objects.equals(dataMatricula, aluno.dataMatricula) && Objects.equals(nomeEscola, aluno.nomeEscola) && Objects.equals(serieMatriculado, aluno.serieMatriculado);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, idade, dataNascimento, registroGeral, numeroCpf, nomeMae, nomePai, dataMatricula, nomeEscola, serieMatriculado, nota1, nota2, nota3, nota4);
+    }
+
+    /**
+     * Methods Calculate Average
+     */
+
+    public double getMediaNota(){
+        double media = (nota1 + nota2 + nota3 + nota4) / 4;
+        return media;
+    }
+
+    public Boolean getAlunoAprovado(){
+        double media = this.getMediaNota();
+        if(media >= 70){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
--- src/br/com/codedevjunior/view/MainActivity.java
+++ src/br/com/codedevjunior/view/MainActivity.java
@@ -1,8 +1,61 @@
 package br.com.codedevjunior.view;
 
+import br.com.codedevjunior.model.Aluno;
+
+import javax.swing.*;
+
 public class MainActivity {
 
     public static void main(String[] args) {
+        /*
+        String nome = JOptionPane.showInputDialog("Qual o nome do aluno? ");
+        String idade = JOptionPane.showInputDialog("Qual a idade do aluno? ");
+        String dataNascimento = JOptionPane.showInputDialog("Qual a data de nascimento do aluno? ");
+        String registroGeral = JOptionPane.showInputDialog("Qual o RG do aluno? ");
+        String numeroCpf = JOptionPane.showInputDialog("Qual o CPF do aluno? ");
+        String nomeMae = JOptionPane.showInputDialog("Qual nome da mae do aluno? ");
+        String nomePai = JOptionPane.showInputDialog("Qual nome do pai do aluno? ");
+        String dataMatricula = JOptionPane.showInputDialog("Qual a data de matricula do aluno? ");
+        String nomeEscola = JOptionPane.showInputDialog("Qual nome da escola do aluno? ");
+        String serieMatriculado = JOptionPane.showInputDialog("Qual a serie do aluno? ");
+        String nota1 = JOptionPane.showInputDialog("Qual a nota 1 do aluno? ");
+        String nota2 = JOptionPane.showInputDialog("Qual a nota 2 do aluno? ");
+        String nota3 = JOptionPane.showInputDialog("Qual a nota 3 do aluno? ");
+        String nota4 = JOptionPane.showInputDialog("Qual a nota 4 do aluno? ");
 
+        Aluno aluno1 = new Aluno();
+        aluno1.setNome(nome);
+        aluno1.setIdade(Integer.parseInt(idade));
+        aluno1.setDataNascimento(dataNascimento);
+        aluno1.setRegistroGeral(registroGeral);
+        aluno1.setNumeroCpf(numeroCpf);
+        aluno1.setNomeMae(nomeMae);
+        aluno1.setNomePai(nomePai);
+        aluno1.setDataMatricula(dataMatricula);
+        aluno1.setNomeEscola(nomeEscola);
+        aluno1.setSerieMatriculado(serieMatriculado);
+        aluno1.setNota1(Double.parseDouble(nota1));
+        aluno1.setNota2(Double.parseDouble(nota2));
+        aluno1.setNota3(Double.parseDouble(nota3));
+        aluno1.setNota4(Double.parseDouble(nota4));
+
+        System.out.println(aluno1.toString());
+        System.out.println(aluno1.getMediaNota());
+        System.out.println(aluno1.getAlunoAprovado());
+         */
+
+        Aluno aluno1 = new Aluno();
+        aluno1.setNome("Vilson");
+        aluno1.setNumeroCpf("092.776.659-04");
+
+        Aluno aluno2 = new Aluno();
+        aluno2.setNome("Vilson");
+        aluno2.setNumeroCpf("092.776.659-04s");
+
+        if(aluno1.equals(aluno2)){
+            System.out.println("São Iguais");
+        } else{
+            System.out.println("São Diferentes");
+        }
     }
 }
